d84a90040f2f68c2887840f59608506ee5f9306b
Abdul1995 <abdul.wahab@twentyzen.com>
2021-01-23 18:11:20 +0100

    Edit download link

---
+++ download.php
@@ -0,0 +1,18 @@
+<?php
+$path = 'assets/ABW-CV.pdf';
+
+$mime_type=mime_content_type($path); 
+ 
+header("Pragma: public");
+header("Expires: 0");
+header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+header("Cache-Control: public");
+header("Content-Description: File Transfer");
+header("Content-Type: " . $mime_type);
+header("Content-Length: " .(string)(filesize($path)) );
+header('Content-Disposition: attachment; filename="'.basename($path).'"');
+header("Content-Transfer-Encoding: binary\n");
+ 
+readfile($path); 
+exit();
+?>
